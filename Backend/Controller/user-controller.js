25dc93ce203bb41a4992f36b8d00f14e64b8ab71
Santosh Alimkar <112555787+Santoshalimkar@users.noreply.github.com>
2023-06-05 20:08:08 +0530

    Update user-controller.js

--- Backend/Controller/user-controller.js
+++ Backend/Controller/user-controller.js
@@ -308,7 +308,7 @@ if (status === "booked") {
 
 await property.save();
 
-      res.redirect(`http://localhost:3000/paymentsuccessful?&orderid=${razorpay_order_id}&proid=${propertyId}&checkin=${checkIn}&checkout=${checkOut}&amount=${amountpaid}`);
+      res.redirect(`https://lonavala-stay.netlify.app/paymentsuccessful?&orderid=${razorpay_order_id}&proid=${propertyId}&checkin=${checkIn}&checkout=${checkOut}&amount=${amountpaid}`);
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Internal server error" });
